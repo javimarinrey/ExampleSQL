ebda0cefe91b934cf13810b56663b25eafbd6a90
jmr <javier.marin@hotusa.com>
2022-08-30 10:10:33 +0200

    commit

--- dates.sql
+++ dates.sql
@@ -2,5 +2,6 @@
 SELECT * FROM HOTEL WHERE FECHA <= TO_DATE('2022/10/14', 'yyyy/mm/dd');
 
 -- TO_CHAR
+-- (expr [, date_format] [, nslparam]);
 SELECT TO_CHAR(SYSDATE, 'HH24:MI:SS') from dual;
 SELECT TO_CHAR(SYSDATE, 'DL', 'NLS_DATE_LANGUAGE = FRENCH') from dual;
